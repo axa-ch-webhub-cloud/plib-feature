dc521f56db74733b9525d010efb241f7c1b2b7d6
Donald Duck <raphael.lueckl@axa.ch>
2019-09-30 08:03:54 +0000

    Deploy Branch: bugfix/testimonials

--- bugfix/testimonials/vendors~main.fb4a25f5bf9f2facbbd6.bundle.js
+++ bugfix/testimonials/vendors~main.fb4a25f5bf9f2facbbd6.bundle.js
@@ -457,4 +457,4 @@ function isObjectObject(o){return!0===
  * Released under the MIT License.
  */
 function isObject(val){return null!=val&&"object"==typeof val&&!1===Array.isArray(val)}(o)&&"[object Object]"===Object.prototype.toString.call(o)}function isPlainObject(o){var ctor,prot;return!1!==isObjectObject(o)&&("function"==typeof(ctor=o.constructor)&&(!1!==isObjectObject(prot=ctor.prototype)&&!1!==prot.hasOwnProperty("isPrototypeOf")))}__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,"default",(function(){return isPlainObject}))}]]);
-//# sourceMappingURL=vendors~main.be3eb67e4e86cee92722.bundle.js.map
+//# sourceMappingURL=vendors~main.fb4a25f5bf9f2facbbd6.bundle.js.map
