590d567c232fc2a944bc6ab96e929ea7bbbc463b
Donald Duck <raphael.lueckl@axa.ch>
2019-11-15 12:02:08 +0000

    Deploy Branch: bugfix/cursor-selection-works-except-double-letters

--- bugfix/cursor-selection-works-except-double-letters/vendors~main.23bdc1eba6c4e6245ff4.bundle.js
+++ bugfix/cursor-selection-works-except-double-letters/vendors~main.23bdc1eba6c4e6245ff4.bundle.js
@@ -457,4 +457,4 @@ function isObjectObject(o){return!0===
  * Released under the MIT License.
  */
 function isObject(val){return null!=val&&"object"==typeof val&&!1===Array.isArray(val)}(o)&&"[object Object]"===Object.prototype.toString.call(o)}function isPlainObject(o){var ctor,prot;return!1!==isObjectObject(o)&&("function"==typeof(ctor=o.constructor)&&(!1!==isObjectObject(prot=ctor.prototype)&&!1!==prot.hasOwnProperty("isPrototypeOf")))}__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,"default",(function(){return isPlainObject}))}]]);
-//# sourceMappingURL=vendors~main.552f38c6f501e21d0ee2.bundle.js.map
+//# sourceMappingURL=vendors~main.23bdc1eba6c4e6245ff4.bundle.js.map
