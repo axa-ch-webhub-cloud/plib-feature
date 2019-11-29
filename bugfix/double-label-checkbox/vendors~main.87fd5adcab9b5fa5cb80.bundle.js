963789ff1575f9553e7b41caa3edb0a74b6bc4cd
Donald Duck <raphael.lueckl@axa.ch>
2019-11-29 11:03:03 +0000

    Deploy Branch: bugfix/double-label-checkbox

--- bugfix/double-label-checkbox/vendors~main.87fd5adcab9b5fa5cb80.bundle.js
+++ bugfix/double-label-checkbox/vendors~main.87fd5adcab9b5fa5cb80.bundle.js
@@ -457,4 +457,4 @@ function isObjectObject(o){return!0===
  * Released under the MIT License.
  */
 function isObject(val){return null!=val&&"object"==typeof val&&!1===Array.isArray(val)}(o)&&"[object Object]"===Object.prototype.toString.call(o)}function isPlainObject(o){var ctor,prot;return!1!==isObjectObject(o)&&("function"==typeof(ctor=o.constructor)&&(!1!==isObjectObject(prot=ctor.prototype)&&!1!==prot.hasOwnProperty("isPrototypeOf")))}__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,"default",(function(){return isPlainObject}))}]]);
-//# sourceMappingURL=vendors~main.b6313d8c8833b6ffc57c.bundle.js.map
+//# sourceMappingURL=vendors~main.87fd5adcab9b5fa5cb80.bundle.js.map
