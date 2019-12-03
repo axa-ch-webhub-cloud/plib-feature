e2f06642fca522fc174d208cd459c073cdf4ff90
Donald Duck <raphael.lueckl@axa.ch>
2019-12-03 15:16:09 +0000

    Deploy Branch: feature/a-better-react-wrapper

--- feature/a-better-react-wrapper/vendors~main.2fc2469cfb69febf8f19.bundle.js
+++ feature/a-better-react-wrapper/vendors~main.2fc2469cfb69febf8f19.bundle.js
@@ -457,4 +457,4 @@ function isObjectObject(o){return!0===
  * Released under the MIT License.
  */
 function isObject(val){return null!=val&&"object"==typeof val&&!1===Array.isArray(val)}(o)&&"[object Object]"===Object.prototype.toString.call(o)}function isPlainObject(o){var ctor,prot;return!1!==isObjectObject(o)&&("function"==typeof(ctor=o.constructor)&&(!1!==isObjectObject(prot=ctor.prototype)&&!1!==prot.hasOwnProperty("isPrototypeOf")))}__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,"default",(function(){return isPlainObject}))}]]);
-//# sourceMappingURL=vendors~main.a9735b3e3858061b35f4.bundle.js.map
+//# sourceMappingURL=vendors~main.2fc2469cfb69febf8f19.bundle.js.map
