c1826421178634c557ab4b217baa0b91f9bf8c0a
Donald Duck <raphael.lueckl@axa.ch>
2020-01-13 10:36:06 +0000

    Deploy Branch: bugfix/checkbox-fixes

--- bugfix/checkbox-fixes/vendors~main.6c55042da2505d7f4f20.bundle.js
+++ bugfix/checkbox-fixes/vendors~main.6c55042da2505d7f4f20.bundle.js
@@ -457,4 +457,4 @@ function isObjectObject(o){return!0===
  * Released under the MIT License.
  */
 function isObject(val){return null!=val&&"object"==typeof val&&!1===Array.isArray(val)}(o)&&"[object Object]"===Object.prototype.toString.call(o)}function isPlainObject(o){var ctor,prot;return!1!==isObjectObject(o)&&("function"==typeof(ctor=o.constructor)&&(!1!==isObjectObject(prot=ctor.prototype)&&!1!==prot.hasOwnProperty("isPrototypeOf")))}__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,"default",(function(){return isPlainObject}))}]]);
-//# sourceMappingURL=vendors~main.aaed0a8ebbfa97e208a5.bundle.js.map
+//# sourceMappingURL=vendors~main.6c55042da2505d7f4f20.bundle.js.map
